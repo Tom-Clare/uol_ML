1e85ef5aa7ee82b97a679739e49c8773d947f806
LazyCrazyTom <tclare50@gmail.com>
2022-02-06 17:31:01 +0000

    working to implement eval_pol_regression

--- script.py
+++ script.py
@@ -63,3 +63,52 @@ plt.plot(x_space, y_predicted6, 'b')
 plt.plot(x_space, y_predicted10, 'm')
 plt.legend(('training points'))
 plt.show()
+
+
+
+count = len(x_train)
+train_split = int(count * 0.7)
+x_train2 = x_train[:train_split]
+y_train2 = y_train[:train_split]
+x_test2 = x_train[train_split:]
+y_test2 = y_train[train_split:]
+
+#w0 = pol_regression(x_train, y_train, 0)
+#x_new0 = getPolynomialDataMatrix(x_space, 0)
+#y_predicted0 = x_new0.dot(w0)
+
+w1 = pol_regression(x_train2, y_train2, 1)
+x_new1 = getPolynomialDataMatrix(x_space, 1)
+y_predicted1 = x_new1.dot(w1)
+
+w2 = pol_regression(x_train2, y_train2, 2)
+x_new2 = getPolynomialDataMatrix(x_space, 2)
+y_predicted2 = x_new2.dot(w2)
+
+w3 = pol_regression(x_train2, y_train2, 3)
+x_new3 = getPolynomialDataMatrix(x_space, 3)
+y_predicted3 = x_new3.dot(w3)
+
+w6 = pol_regression(x_train2, y_train2, 6)
+x_new6 = getPolynomialDataMatrix(x_space, 6)
+y_predicted6 = x_new6.dot(w6)
+
+w10 = pol_regression(x_train2, y_train2, 10)
+x_new10 = getPolynomialDataMatrix(x_space, 10)
+y_predicted10 = x_new10.dot(w10)
+
+def eval_pol_regression(parameters, x, y, degree):
+    #for i in parameters:
+        #y_predicted = parameters[i]
+        #y_test = y[i]
+        #x_test = x[i]
+        # work out rmse
+    
+    x_new1 = getPolynomialDataMatrix(x_space, degree)
+    y_predicted = x_new1.dot(parameters)
+
+    return parameters
+
+print(eval_pol_regression(w1, x_test2, y_test2, 1))
+
+
